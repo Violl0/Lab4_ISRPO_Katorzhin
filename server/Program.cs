e2139f3d7a2c61ee2c78499abf311183e1404236
Violl0 <maxim99911@bk.ru>
2026-09-24 09:41:23 +0300

    Add_server

---
+++ server/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+Console.WriteLine("пр!");
+Console.WriteLine("1 — Показать ФИО");
+Console.WriteLine("2 — Показать группу");
+Console.WriteLine("3 — Показать дату");
+Console.WriteLine("4 — Выход");
+int a = int.Parse(Console.ReadLine());
+switch (a){
+    case 1:
+        Console.WriteLine("Каторжин Максим Вячеславович");
+        break;
+    case 2:
+        Console.WriteLine("ИСП-241");
+        break;
+    case 3:
+        Console.WriteLine("24.09 9:21");
+        break;
+    case 4:
+        break;
+}
+
